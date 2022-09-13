05a1761b6b5cb1d69ccbe8ab8d7f848535e4c675
zdenkotraste <zhraste98@gmail.com>
2022-09-13 08:42:08 -0400

    cambio en index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Zdenko" }');
+    res.send('{ "response": "Hello From EducacionIT" }');
 });
 
 app.get('/will', function (req, res) {
